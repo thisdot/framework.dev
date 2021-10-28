e94e21a1d93168764008bb2a8b6f151906f182f3
Tom VanAntwerp <tvanantwerp@users.noreply.github.com>
2021-10-28 14:07:11 -0400

    Add gray color palette from mockups (#17)
    * Bring in color palette from Figma mockups
    
    Only gray I could not find in use was 400
    
    * Found gray 400

--- packages/system/src/themes/themes.css.ts
+++ packages/system/src/themes/themes.css.ts
@@ -1,9 +1,21 @@
 import { createTheme, createThemeContract } from "@vanilla-extract/css"
 
+export const themeColors = {
+	gray100: "#F7F9FA",
+	gray200: "#EDEFF2",
+	gray300: "#D3DDE6",
+	gray400: "#B4BFCC",
+	gray500: "#95A1AD",
+	gray600: "#78838F",
+	gray700: "#4D545C",
+	gray800: "#25292E",
+	gray900: "#1F2529",
+}
+
 export const vars = createThemeContract({
 	color: {
-		background: "",
-		text: "",
+		background: themeColors.gray100,
+		text: themeColors.gray700,
 		primary: "",
 		primaryText: "",
 	},
@@ -11,17 +23,17 @@ export const vars = createThemeContract({
 
 export const reactTheme = createTheme(vars, {
 	color: {
-		background: "white",
-		text: "black",
-		primary: "black",
+		background: themeColors.gray100,
+		text: themeColors.gray700,
+		primary: themeColors.gray900,
 		primaryText: "white",
 	},
 })
 
 export const vueTheme = createTheme(vars, {
 	color: {
-		background: "white",
-		text: "black",
+		background: themeColors.gray100,
+		text: themeColors.gray900,
 		primary: "green",
 		primaryText: "white",
 	},
