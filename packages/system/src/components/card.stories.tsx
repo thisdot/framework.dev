8ea4b6f3fe6a5f9851c556b97afbfab997d8d73a
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-10-29 09:53:21 -0400

    Add background color to card in storybook

--- packages/system/src/components/card.stories.tsx
+++ packages/system/src/components/card.stories.tsx
@@ -1,9 +1,23 @@
 import { Story, Meta } from "@storybook/react"
+
 import { Card as CardComponent, CardProps } from "./card"
+import { themeColors } from "../themes/themes.css"
 
 export default {
 	title: "Card",
 	component: CardComponent,
+	parameters: {
+		backgrounds: {
+			default: "Light Theme Background",
+			values: [
+				{
+					name: "Light Theme Background",
+					value: themeColors.gray100,
+					default: true,
+				},
+			],
+		},
+	},
 } as Meta
 
 const Template: Story<CardProps> = (args) => <CardComponent {...args} />
