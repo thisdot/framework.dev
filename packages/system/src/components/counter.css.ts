575d4c5e22f1288edee145c406ce1e2c0c4c3621
Tom VanAntwerp <tvanantwerp@users.noreply.github.com>
2021-10-29 15:50:06 -0400

    Fix counter styles given changes to sprinkles (#21)

--- packages/system/src/components/counter.css.ts
+++ packages/system/src/components/counter.css.ts
@@ -1,10 +1,15 @@
-import { sprinkles } from "../sprinkles/sprinkles.css"
+import { style } from "@vanilla-extract/css"
 
-export const counterStyle = sprinkles({
-	borderRadius: 24,
-	color: "primaryButton",
-	fontSize: "base",
-	fontWeight: "bold",
-	paddingX: 10,
-	paddingY: 4,
-})
+import { sprinkles } from "../sprinkles/sprinkles.css"
+import { themeColors } from "../themes/themes.css"
+
+export const counterStyle = style([
+	sprinkles({
+		borderRadius: 24,
+		fontSize: "base",
+		fontWeight: "bold",
+		paddingX: 10,
+		paddingY: 4,
+	}),
+	{ backgroundColor: themeColors.gray900, color: "#fff" },
+])
