bea8faece49c7cd24c04d2d374fed1e519d0e217
Jae Anne Bach Hardie <jabh@hey.com>
2021-11-02 17:07:28 -0400

    Add navigation

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
 	packageOptions: {
 		knownEntrypoints: [
 			"classnames",
+			"@reach/dialog",
 			"@vanilla-extract/sprinkles/createRuntimeSprinkles",
+			"@vanilla-extract/recipes/createRuntimeFn",
 			"the-new-css-reset/css/reset.css",
 			"normalize.css",
 		],
