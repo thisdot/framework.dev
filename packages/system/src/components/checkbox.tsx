7aeef06b89edeabaa3dd05667faa5ba04a555591
Jae Anne Bach Hardie <jabh@hey.com>
2021-11-09 12:46:07 -0500

    Add checkbox component (#81)
    * Correlate input labels with IDs
    
    * Add checkbox component
    
    * Remove unusued checkbox styles

---
+++ packages/system/src/components/checkbox.tsx
@@ -0,0 +1,31 @@
+import classNames from "classnames"
+import React from "react"
+import { useId } from "@reach/auto-id"
+import { sprinkles } from "../sprinkles/sprinkles.css"
+import { checkboxLabelStyle } from "./checkbox.css"
+
+export interface CheckboxProps extends React.ComponentPropsWithoutRef<"input"> {
+	label: string
+}
+
+export function Checkbox({
+	className,
+	label,
+	id: customIdFromProps,
+	...props
+}: CheckboxProps) {
+	const id = useId(customIdFromProps)
+	return (
+		<div className={className}>
+			<input
+				type="checkbox"
+				className={sprinkles({ hidden: "visually" })}
+				id={id}
+				{...props}
+			/>
+			<label htmlFor={id} className={checkboxLabelStyle}>
+				{label}
+			</label>
+		</div>
+	)
+}
