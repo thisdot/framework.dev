bb42edf1476415b94add7999d24319f8e6bdadf4
Jae Anne Bach Hardie <jabh@hey.com>
2021-11-09 12:39:27 -0500

    Add NavItem component

---
+++ packages/system/src/components/nav-item.stories.tsx
@@ -0,0 +1,24 @@
+import { Story, Meta } from "@storybook/react"
+import { InteractiveIcon } from "../icons/interactive-icon"
+import { NavItem as NavItemComponent, NavItemProps } from "./nav-item"
+
+export default {
+	title: "Nav Item",
+	component: NavItemComponent,
+	args: {
+		icon: InteractiveIcon,
+		children: "Menu Item",
+		count: 24
+	},
+	argTypes: {
+		"aria-current": {
+			options: [undefined, "page"],
+			control: { type: "radio" },
+			defaultValue: undefined
+		},
+	}
+} as Meta
+
+const Template: Story<NavItemProps> = (args) => <NavItemComponent {...args} />
+
+export const NavItem = Template.bind({})
