44f18befb76e95205ef9fde2f42adb22e67badb6
Jae Anne Bach Hardie <jabh@hey.com>
2021-11-10 17:18:04 -0500

    Add Code Example card (#88)

---
+++ packages/system/src/components/code-example-card.tsx
@@ -0,0 +1,64 @@
+import classNames from "classnames"
+import React from "react"
+import { kebabCase } from "lodash"
+import { sprinkles } from "../sprinkles/sprinkles.css"
+import { CodeExample } from "../models/code-example"
+import {
+	codeExampleCardStyle,
+	codeExampleCardHeaderStyle,
+	codeExampleCardNameStyle,
+	codeExampleCardBodyStyle,
+} from "./code-example-card.css"
+import { Tag } from "./tag"
+
+export type CodeExampleCardProps = React.ComponentPropsWithoutRef<"article"> & {
+	headingTag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+	codeExample: CodeExample<string>
+}
+
+export function CodeExampleCard({
+	codeExample,
+	headingTag: H,
+	className,
+	...props
+}: CodeExampleCardProps) {
+	return (
+		<article className={classNames(className, codeExampleCardStyle)} {...props}>
+			<header className={codeExampleCardHeaderStyle}>
+				<div className={codeExampleCardNameStyle}>
+					<a href={codeExample.href} target="_blank" rel="noreferrer">
+						<H
+							className={sprinkles({
+								margin: 0,
+								textStyle: "bodyShort1",
+								color: "strongText",
+								fontWeight: "bold",
+							})}
+						>
+							{codeExample.title}
+						</H>
+					</a>
+					<p
+						className={sprinkles({
+							textStyle: "bodyShort3",
+							color: "softText",
+						})}
+					>
+						{codeExample.author}
+					</p>
+				</div>
+			</header>
+			<p className={codeExampleCardBodyStyle}>{codeExample.description}</p>
+			<div className={sprinkles({ layout: "row", gap: 4, paddingTop: 12, borderTop: "light" })}>
+				{codeExample.tags.map((tag) => (
+					<Tag
+						key={tag}
+						href={`/categories/code-examples/tags/${kebabCase(tag)}`}
+					>
+						{tag}
+					</Tag>
+				))}
+			</div>
+		</article>
+	)
+}
