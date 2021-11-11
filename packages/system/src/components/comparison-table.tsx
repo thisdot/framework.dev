094d7c6378b9a2c62349ab5c5b9312346ee284d5
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-11-11 15:41:53 -0500

    Fetch GitHub stars via shields.io
    Not using the GitHub API itself due to IP rate limiting while unauthenticated

--- packages/system/src/components/comparison-table.tsx
+++ packages/system/src/components/comparison-table.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Library } from "../models/library"
+import { getGitHubStarsUrl } from "../util/stats-util"
 import {
 	comparisonTableContainerStyle,
 	comparisonTableStyle,
@@ -26,46 +27,74 @@ export interface ComparisonTableProps
 	libraries: Library<string>[]
 }
 
+interface LibraryWithData extends Library<string> {
+	stars: string
+}
+
 export function ComparisonTable({
 	libraries,
 	className,
 	...props
 }: ComparisonTableProps) {
+	const [librariesWithData, setLibrariesWithData] = useState<LibraryWithData[]>(
+		[]
+	)
+	useEffect(() => {
+		async function fetchData() {
+			const librariesWithData = await Promise.all(
+				libraries.map(async (library) => {
+					const stars = await fetch(
+						getGitHubStarsUrl(library.gitHubRepo, "json")
+					)
+						.then((res) => res.json())
+						.then((res) => res.value)
+					return { ...library, stars }
+				})
+			)
+			setLibrariesWithData(librariesWithData)
+		}
+		fetchData()
+	}, [libraries])
+
+	console.log(librariesWithData)
+
 	return (
 		<div
 			className={classNames(className, comparisonTableContainerStyle)}
 			{...props}
 		>
-			<table className={comparisonTableStyle}>
-				<thead className={comparisonTableHeadStyle}>
-					<tr>
-						<TH>Name</TH>
-						<TH>Author</TH>
-						<TH>Testing Coverage</TH>
-						<TH>Weekly Downloads</TH>
-						<TH>Overall Health</TH>
-						<TH>Stars</TH>
-					</tr>
-				</thead>
-				<tbody>
-					{libraries.map((library) => (
-						<tr key={library.name} className={comparisonTableRowCellStyle}>
-							<TD>
-								<img
-									src={library.image}
-									className={comparisonTableLibraryIconStyle}
-								/>
-								{library.name}
-							</TD>
-							<TD>{library.author}</TD>
-							<TD>85%</TD>
-							<TD>29M/week</TD>
-							<TD>92%</TD>
-							<TD>2.4K</TD>
+			{librariesWithData && (
+				<table className={comparisonTableStyle}>
+					<thead className={comparisonTableHeadStyle}>
+						<tr>
+							<TH>Name</TH>
+							<TH>Author</TH>
+							<TH>Testing Coverage</TH>
+							<TH>Weekly Downloads</TH>
+							<TH>Overall Health</TH>
+							<TH>Stars</TH>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{librariesWithData.map((library) => (
+							<tr key={library.name} className={comparisonTableRowCellStyle}>
+								<TD>
+									<img
+										src={library.image}
+										className={comparisonTableLibraryIconStyle}
+									/>
+									{library.name}
+								</TD>
+								<TD>{library.author}</TD>
+								<TD>85%</TD>
+								<TD>29M/week</TD>
+								<TD>92%</TD>
+								<TD>{library.stars}</TD>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)}
 		</div>
 	)
 }
