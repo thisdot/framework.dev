fab6e7ce992a744b9c29d599554f5c532900fa62
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-11-12 11:23:38 -0500

    Sort when clicking TH

--- packages/system/src/components/comparison-table.tsx
+++ packages/system/src/components/comparison-table.tsx
@@ -11,14 +11,18 @@ import {
 	comparisonTableRowCellStyle,
 } from "./comparison-table.css"
 
-const TH = ({ children, className }: React.ComponentPropsWithoutRef<"th">) => (
-	<th className={classNames(comparisonTableCellStyle, className)}>
+const TH = ({
+	children,
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<"th">) => (
+	<th className={classNames(comparisonTableCellStyle, className)} {...props}>
 		{children}
 	</th>
 )
 
-const TD = ({ children }: React.ComponentPropsWithoutRef<"td">) => (
-	<td className={comparisonTableCellStyle}>
+const TD = ({ children, ...props }: React.ComponentPropsWithoutRef<"td">) => (
+	<td className={comparisonTableCellStyle} role="button" {...props}>
 		<div className={comparisonTableCellContentsStyle}>{children}</div>
 	</td>
 )
@@ -28,19 +32,20 @@ export interface ComparisonTableProps
 	libraries: Library<string>[]
 }
 
-interface LibraryWithData extends Library<string> {
-	stars: string
-}
-
 export function ComparisonTable({
 	libraries,
 	className,
 	...props
 }: ComparisonTableProps) {
-	const [libraryStats, setLibraryStats] = useState<LibraryWithData[]>([])
+	const [libraryStats, setLibraryStats] = useState([])
+	const [sortBy, setSortBy] = useState<
+		"name" | "author" | "coverage" | "downloads" | "health" | "stars"
+	>("name")
+	const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc")
+
 	useEffect(() => {
 		async function fetchData() {
-			const data = await fetch(`https://api.npms.io/v2/package/mget`, {
+			const npmsio = await fetch(`https://api.npms.io/v2/package/mget`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
@@ -48,10 +53,51 @@ export function ComparisonTable({
 				},
 				body: JSON.stringify(libraries.map((library) => library.npmPackage)),
 			}).then((res) => res.json())
+			const data = libraries.map((library) => {
+				return {
+					name: library.name,
+					author: library.author,
+					image: library.image,
+					href: library.href,
+					coverage: npmsio[library.npmPackage]?.evaluation?.quality?.tests,
+					downloads:
+						npmsio[library.npmPackage]?.collected?.npm?.downloads[1]?.count,
+					health: npmsio[library.npmPackage]?.evaluation?.quality?.health,
+					stars: npmsio[library.npmPackage]?.collected?.github?.starsCount,
+				}
+			})
 			setLibraryStats(data)
 		}
 		fetchData()
 	}, [libraries])
+
+	useEffect(() => {
+		if (libraryStats.length === 0) {
+			return
+		}
+		const sorted = [...libraryStats].sort((a, b) => {
+			if (a[sortBy] === b[sortBy]) {
+				return 0
+			}
+			if (sortDirection === "asc") {
+				return a[sortBy] > b[sortBy] ? 1 : -1
+			} else {
+				return b[sortBy] > a[sortBy] ? 1 : -1
+			}
+		})
+		setLibraryStats(sorted)
+	}, [sortBy, sortDirection])
+
+	function handleSort(heading: typeof sortBy) {
+		if (sortBy === heading) {
+			setSortDirection(sortDirection === "asc" ? "desc" : "asc")
+		} else {
+			setSortBy(heading)
+			setSortDirection(
+				heading === "name" || heading === "author" ? "asc" : "desc"
+			)
+		}
+	}
 
 	return (
 		<div
@@ -62,16 +108,16 @@ export function ComparisonTable({
 				<table className={comparisonTableStyle}>
 					<thead className={comparisonTableHeadStyle}>
 						<tr>
-							<TH>Name</TH>
-							<TH>Author</TH>
-							<TH>Testing Coverage</TH>
-							<TH>Weekly Downloads</TH>
-							<TH>Overall Health</TH>
-							<TH>Stars</TH>
+							<TH onClick={() => handleSort("name")}>Name</TH>
+							<TH onClick={() => handleSort("author")}>Author</TH>
+							<TH onClick={() => handleSort("coverage")}>Testing Coverage</TH>
+							<TH onClick={() => handleSort("downloads")}>Weekly Downloads</TH>
+							<TH onClick={() => handleSort("health")}>Overall Health</TH>
+							<TH onClick={() => handleSort("stars")}>Stars</TH>
 						</tr>
 					</thead>
 					<tbody>
-						{libraries.map((library) => (
+						{libraryStats.map((library) => (
 							<tr key={library.name} className={comparisonTableRowCellStyle}>
 								<TD>
 									<img
@@ -87,22 +133,10 @@ export function ComparisonTable({
 									</a>
 								</TD>
 								<TD>{library.author}</TD>
-								<TD>
-									{libraryStats[library.npmPackage]?.evaluation?.quality
-										?.tests || "N/A"}
-								</TD>
-								<TD>
-									{libraryStats[library.npmPackage]?.collected?.npm
-										?.downloads[1]?.count || "N/A"}
-								</TD>
-								<TD>
-									{libraryStats[library.npmPackage]?.evaluation?.quality
-										?.health || "N/A"}
-								</TD>
-								<TD>
-									{libraryStats[library.npmPackage]?.collected?.github
-										?.starsCount || "N/A"}
-								</TD>
+								<TD>{library.coverage || "N/A"}</TD>
+								<TD>{library.downloads || "N/A"}</TD>
+								<TD>{library.health || "N/A"}</TD>
+								<TD>{library.stars || "N/A"}</TD>
 							</tr>
 						))}
 					</tbody>
