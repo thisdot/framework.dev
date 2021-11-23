748399f9e0f1b831cb83922b3ffbfada87ab3e6c
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-11-23 09:47:05 -0500

    Scroll buttons functional

--- packages/system/src/components/comparison-table/components/horizontal-scrollbar.tsx
+++ packages/system/src/components/comparison-table/components/horizontal-scrollbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import classNames from "classnames"
 import {
 	horizontalScrollbarContainerStyle,
@@ -19,10 +19,18 @@ export const HorizontalScrollbar = ({
 	const scrollSectionRef = useRef<HTMLDivElement>(null)
 	const [thumbWidth, setThumbWidth] = useState(5)
 
-	function handleResize(ref) {
+	function handleResize(ref: HTMLDivElement) {
 		const { clientWidth, scrollWidth } = ref
 		const visibleRatio = clientWidth / scrollWidth
 		setThumbWidth(Math.max(visibleRatio * 100, 5))
+	}
+
+	function handleScrollButton(direction: "left" | "right") {
+		const { current } = scrollSectionRef
+		if (current) {
+			const scrollAmount = direction === "right" ? 200 : -200
+			current.scrollBy({ left: scrollAmount, behavior: "smooth" })
+		}
 	}
 
 	useEffect(() => {
@@ -52,10 +60,16 @@ export const HorizontalScrollbar = ({
 					></div>
 				</div>
 				<div className={horizontalScrollbarButtonContainerStyle}>
-					<button className={horizontalScrollbarButtonStyle}>
+					<button
+						className={horizontalScrollbarButtonStyle}
+						onClick={() => handleScrollButton("left")}
+					>
 						<ChevronIcon />
 					</button>
-					<button className={horizontalScrollbarButtonStyle}>
+					<button
+						className={horizontalScrollbarButtonStyle}
+						onClick={() => handleScrollButton("right")}
+					>
 						<ChevronIcon flipped={true} />
 					</button>
 				</div>
