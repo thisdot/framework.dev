c47049c96eae8e2211ae3e82242e53239c442510
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-11-30 09:34:10 -0500

    Remove unused native scroll styles

--- packages/system/src/components/comparison-table/comparison-table.css.ts
+++ packages/system/src/components/comparison-table/comparison-table.css.ts
@@ -1,6 +1,5 @@
 import { style } from "@vanilla-extract/css"
 import { sprinkles } from "../../sprinkles/sprinkles.css"
-import { vars } from "../../themes/themes.css"
 import { pxToRem } from "../../util/style-utils"
 
 export const comparisonTableStyle = style([
@@ -13,22 +12,6 @@ export const comparisonTableStyle = style([
 	{
 		gridTemplateColumns: "max-content repeat(5, 1fr)",
 		position: "relative",
-		"::-webkit-scrollbar": {
-			backgroundColor: "white",
-			borderBottomLeftRadius: pxToRem(12),
-			borderBottomRightRadius: pxToRem(12),
-			height: pxToRem(30 * 2 + 4),
-		},
-		"::-webkit-scrollbar-track": {
-			backgroundColor: vars.palette.neutral80,
-			border: "30px solid rgba(255, 255, 255, 0)",
-			backgroundClip: "padding-box",
-		},
-		"::-webkit-scrollbar-thumb": {
-			backgroundColor: vars.palette.neutral20,
-			border: "30px solid rgba(255, 255, 255, 0)",
-			backgroundClip: "padding-box",
-		},
 	},
 ])
 
