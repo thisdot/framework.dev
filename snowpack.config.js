90da7b8521ca00430240f16db1318e44c3049435
Jae Anne Bach Hardie <jabh@hey.com>
2021-11-30 17:34:18 -0500

    Fix astro build by removing reach/utils

--- snowpack.config.js
+++ snowpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
 			"@reach/utils/make-id",
 			"@reach/utils/use-update-effect",
 			"@reach/utils/use-stateful-ref-value",
+			"@reach/tooltip",
+			"@reach/utils/get-document-dimensions",
 		],
 		env: {
 			NODE_ENV: true,
