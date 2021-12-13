fbf3f9d82aed6ad4593c03fd2083dcbecac66929
Jae Anne Bach Hardie <jabh@hey.com>
2021-12-13 11:56:14 -0500

    Add the capability to search the site from a searchbox in Google results

--- packages/react/src/components/search.tsx
+++ packages/react/src/components/search.tsx
@@ -2,8 +2,22 @@ import {
 	Search as _Search,
 	SearchProps,
 } from "@framework/system/src/components/search/search"
+import { useEffect, useState } from "react"
 import { searchData } from "../data/search-data"
 
 export default function Search(props: SearchProps) {
-	return <_Search {...props} data={searchData} />
+	const [initialQuery, setInitialQuery] = useState("")
+	useEffect(() => {
+		setInitialQuery(
+			new URLSearchParams(document.location.search).get("q") ?? ""
+		)
+	}, [])
+	return (
+		<_Search
+			{...props}
+			data={searchData}
+			initialQuery={initialQuery}
+			key={initialQuery ? "client-loaded" : "ssr"}
+		/>
+	)
 }
