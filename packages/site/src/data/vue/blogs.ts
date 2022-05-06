6a2e176d3e7fd64064aa0b93e5a40f75d647c170
jesus4497 <jesuspadron4497@gmail.com>
2022-05-06 15:29:07 -0400

    feat: add new content for vue site

--- packages/site/src/data/vue/blogs.ts
+++ packages/site/src/data/vue/blogs.ts
@@ -2,4 +2,13 @@ import { Blog } from "@framework/system/src/models/blog"
 
 export const blogTags = [] as const
 
-export const blogs: Blog<typeof blogTags[number]>[] = []
+export const blogs: Blog<typeof blogTags[number]>[] = [
+	{
+		title: "The Vue Point",
+		author: "Evan You",
+		description: "Updates, tips & opinions from the maintainers of Vue.js.",
+		image: "https://github.com/yyx990803.png",
+		href: "https://blog.vuejs.org/",
+		tags: [],
+	},
+]
--- packages/site/src/data/vue/tools.ts
+++ packages/site/src/data/vue/tools.ts
@@ -1,5 +1,20 @@
 import { Tool } from "@framework/system/src/models/tool"
 
-export const toolTags = [] as const
+export const toolTags = [
+    "VSCode",
+	"Chrome",
+	"Firefox",
+	"Safari",
+	"UI Design",
+] as const
 
-export const tools: Tool<typeof toolTags[number]>[] = []
+export const tools: Tool<typeof toolTags[number]>[] = [
+    {
+		name: "Vue.js devtools for Chrome",
+		author: "Vue.js",
+		description: "Browser DevTools extension for debugging Vue.js applications.",
+		image: "https://github.com/vuejs.png",
+		href: "https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd?hl=en",
+		tags: ["Chrome"],
+	},
+]
