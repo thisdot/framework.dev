c8f20f214244a11e61dcaae7c715ca5284da85ad
Jesus Padron <jesuspadron4497@gmail.com>
2022-05-06 18:54:18 -0400

    Vue - Feature: add new content for vue site (#195)
    * feat: add new content for vue site
    
    * chore: update changelog

--- packages/site/src/data/vue/books.ts
+++ packages/site/src/data/vue/books.ts
@@ -1,5 +1,24 @@
 import { Book } from "@framework/system/src/models/book"
 
-export const bookTags = [] as const
+export const bookTags = [
+    'projects',
+    'vuex',
+    'routing',
+    'testing',
+	"Vue.js 2",
+    "Vue.js 3",
+] as const
 
-export const books: Book<typeof bookTags[number]>[] = []
+export const books: Book<typeof bookTags[number]>[] = [
+    {
+		title: "Vue.js: Up and Running",
+		authors: ["Callum Macrae"],
+		image: "https://learning.oreilly.com/library/cover/9781491997239/250w/",
+		description: "Get a brisk introduction to building fast, interactive single-page web applications with Vue.js, the popular JavaScript framework that organizes and simplifies web development. With this practical guide, you'll quickly move from basics to custom components and advanced features—including JSX, the JavaScript syntax extension.",
+		yearOfPublication: 2018,
+		numberOfPages: 174,
+		level: 'beginner',
+		href: 'https://www.oreilly.com/library/view/vuejs-up-and/9781491997239/',
+		tags: ['projects', 'vuex', 'routing', 'testing', "Vue.js 2"]
+	},
+]
--- packages/site/src/data/vue/courses.ts
+++ packages/site/src/data/vue/courses.ts
@@ -1,5 +1,35 @@
 import { Course } from "@framework/system/src/models/course"
 
-export const courseTags = [] as const
+export const courseTags = [
+    "state management",
+	"data fetching",
+	"routing",
+	"testing",
+	"forms",
+	"graphQL",
+	"vuex",
+	"authentication",
+	"scss",
+	"css",
+	"bootstrap",
+	"typescript",
+	"tailwind",
+	"material ui",
+    "Vue.js 2",
+    "Vue.js 3",
+] as const
 
-export const courses: Course<typeof courseTags[number]>[] = []
+export const courses: Course<typeof courseTags[number]>[] = [
+    {
+        title: "Vue - The Complete Guide (incl. Router & Composition API)",
+		author: "Maximilian Schwarzmüller",
+		image: "https://github.com/maxschwarzmueller.png",
+		description:
+			"Join this bestselling Vue course to learn how to use the latest version of VueJS to build amazing, modern web apps!",
+		paymentType: "paid",
+		level: "beginner",
+		format: "video",
+		href: "https://www.udemy.com/course/vuejs-2-the-complete-guide/",
+		tags: ["state management", "data fetching", "routing", "Vue.js 2", "Vue.js 3", "vuex", "authentication", "forms"],
+    }
+]
