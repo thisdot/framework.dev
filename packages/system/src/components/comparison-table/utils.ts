f928bd9d9b0f9d2a1ca342b77c9dbe5256f5c624
jesus4497 <jesuspadron4497@gmail.com>
2022-06-24 17:30:52 -0400

    fix: table sort case sensitiveD

--- packages/system/src/components/comparison-table/utils.ts
+++ packages/system/src/components/comparison-table/utils.ts
@@ -4,14 +4,24 @@ export function sortLibraries(
 	libraries: ILibrary[],
 	sortConfig: ISortConfig
 ): ILibrary[] {
-	return [...libraries].sort((a, b) => {
-		if (a[sortConfig.by] === b[sortConfig.by]) {
+	return libraries.sort((a, b) => {
+		const valueA =
+			typeof a[sortConfig.by] === "string"
+				? (a[sortConfig.by] as string).toUpperCase()
+				: a[sortConfig.by]
+
+		const valueB =
+			typeof b[sortConfig.by] === "string"
+				? (b[sortConfig.by] as string).toUpperCase()
+				: b[sortConfig.by]
+
+		if (valueA === valueB) {
 			return 0
 		}
 		if (sortConfig.asc) {
-			return a[sortConfig.by] > b[sortConfig.by] ? 1 : -1
+			return valueA > valueB ? 1 : -1
 		} else {
-			return b[sortConfig.by] > a[sortConfig.by] ? 1 : -1
+			return valueB > valueA ? 1 : -1
 		}
 	})
 }
