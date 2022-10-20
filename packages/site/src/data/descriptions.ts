1bfca1cd31e3b3d495bcbd46a9d75d83b2e29407
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2022-10-20 12:25:01 +0000

    Feat/qwik pod 276 (#313)
    * Added dynamic social image
    
    * formatting
    
    * added host url
    
    * Added descriptions for the individual frameworks.

---
+++ packages/site/src/data/descriptions.ts
@@ -0,0 +1,8 @@
+export const descriptions = {
+    "angular": "",
+    "react": "",
+    "solidjs": "",
+    "vue": "",
+    "svelte": "",
+    "qwik": "No hydration, auto lazy-loading, edge-optimized, and fun 🎉!"
+}
