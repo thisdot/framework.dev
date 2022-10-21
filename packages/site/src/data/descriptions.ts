479f7295be00e0774b0d0732a3f6472261b3d2a1
Maarten Bicknese <maarten.bicknese@thisdot.co>
2022-10-21 14:37:28 +0200

    chore(solidjs): add description for solidjs

--- packages/site/src/data/descriptions.ts
+++ packages/site/src/data/descriptions.ts
@@ -1,7 +1,7 @@
 export const descriptions = {
     "angular": "",
     "react": "",
-    "solidjs": "",
+    "solidjs": "Simple and performant reactivity for building user interfaces.",
     "vue": "",
     "svelte": "Svelte is a radical new approach to building user interfaces, with less boilerplate and reactive variables.",
     "qwik": "No hydration, auto lazy-loading, edge-optimized, and fun 🎉!"
