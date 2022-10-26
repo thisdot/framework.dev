0d78f62529e4c101606e3d4eed0f338c5451e265
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2022-10-26 19:18:14 +0000

    Revert "Add framework on the main framework.dev site banner (#326)"
    This reverts commit be6ac5950a3c6dc8b0d940c1d83d1feb50081e8f.

--- packages/system/src/globals/global-constants.tsx
+++ packages/system/src/globals/global-constants.tsx
@@ -2,7 +2,6 @@ import { LinkCardProps } from "@framework/system/src/components/landing/link-car
 import { AngularIcon } from "../icons/angular-icon"
 import { ReactIcon } from "../icons/react-icon"
 import { VueIcon } from "../icons/vue-icon"
-import { QwikIcon } from "../icons/qwik-icon"
 
 export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 	{
@@ -30,15 +29,6 @@ export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 			return <VueIcon {...props} />
 		},
 		backgroundColor: "#41B883",
-		bigSize,
-	},
-	{
-		title: "Qwik Resources",
-		href: "https://qwik.framework.dev/",
-		Icon(props) {
-			return <QwikIcon {...props} />
-		},
-		backgroundColor: "#5468ff",
 		bigSize,
 	},
 ]
