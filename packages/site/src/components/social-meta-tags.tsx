dcad566726d272b929a4e8b37faa06f72ab9796d
morgnism <morgan@border.dev>
2022-10-28 16:40:35 -0400

    fix meta description

--- packages/site/src/components/social-meta-tags.tsx
+++ packages/site/src/components/social-meta-tags.tsx
@@ -20,7 +20,7 @@ export function SocialMetaTags({ siteName, title }: SocialMetaTagsProps) {
 	return (
 		<>
 			<meta name="title" content={title} />
-			<meta name="description" content="{description}" />
+			<meta name="description" content={description} />
 
 			<meta property="og:type" content="website" />
 			<meta property="og:url" content={url} />
