6384d0bf9e4f82127cc97288209942d3031c003e
Chinedu Ekene Okpala <allstackdev@gmail.com>
2022-10-28 13:05:59 +0000

    remove test data

--- packages/site/src/data/qwik/courses.ts
+++ packages/site/src/data/qwik/courses.ts
@@ -7,16 +7,16 @@ export const courseTags = [
 ] as const
 
 export const courses: Course<typeof courseTags[number]>[] = [
-  {
-		title: "test",
-		author: "test",
-		image: "https://github.com/john-smilga.png",
-		description:
-			"test test test test.",
-		paymentType: "free",
-		level: "advanced",
-		format: "video",
-		href: "https://www.youtube.com/watch?v=a_7Z7C_JCyo",
-		tags: ["state management", "data fetching", "routing"],
-	},
+//   {
+// 		title: "test",
+// 		author: "test",
+// 		image: "https://github.com/john-smilga.png",
+// 		description:
+// 			"test test test test.",
+// 		paymentType: "free",
+// 		level: "advanced",
+// 		format: "video",
+// 		href: "https://www.youtube.com/watch?v=a_7Z7C_JCyo",
+// 		tags: ["state management", "data fetching", "routing"],
+// 	},
 ]
