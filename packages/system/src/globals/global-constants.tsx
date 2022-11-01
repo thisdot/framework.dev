9be1a8a0f8d4b33b9bf10cb48c3f8d996bfeede7
jdwilkin4 <jwilkin4@hotmail.com>
2022-11-01 16:55:26 -0700

    feat: adding nodejs to main site

--- packages/system/src/globals/global-constants.tsx
+++ packages/system/src/globals/global-constants.tsx
@@ -4,6 +4,7 @@ import { ReactIcon } from "../icons/react-icon"
 import { VueIcon } from "../icons/vue-icon"
 import { SolidJSIcon } from "../icons/solidjs-icon"
 import { QwikIcon } from "../icons/qwik-icon"
+import { NodejsIcon } from "../icons/nodejs-icon"
 
 export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 	{
@@ -34,21 +35,30 @@ export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 		bigSize,
 	},
 	{
-	        title: "SolidJS Resources",
+		title: "SolidJS Resources",
 		href: "https://solidjs.framework.dev/",
 		Icon(props) {
 			return <SolidJSIcon {...props} />
 		},
 		backgroundColor: "#2c4f7c",
 		bigSize,
-  	},
-  	{
+	},
+	{
 		title: "Qwik Resources",
 		href: "https://qwik.framework.dev/",
 		Icon(props) {
 			return <QwikIcon {...props} />
 		},
 		backgroundColor: "#5468ff",
+		bigSize,
+	},
+	{
+		title: "NodeJS Resources",
+		href: "https://nodejs.framework.dev/",
+		Icon(props) {
+			return <NodejsIcon {...props} />
+		},
+		backgroundColor: "#539e43",
 		bigSize,
 	},
 ]
