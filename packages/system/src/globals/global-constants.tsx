7729791b9507a646c4d0359936063c0ff532129e
Morgan Worrell <morgan@thisdot.co>
2022-11-07 07:44:27 -0700

    Fix/389 missing hero images (#428)
    * enabled solidjs linking on landing page
    
    * fixed landing page solidjs clour

--- packages/system/src/globals/global-constants.tsx
+++ packages/system/src/globals/global-constants.tsx
@@ -3,8 +3,8 @@ import { AngularIcon } from "../icons/angular-icon"
 import { QwikIcon } from "../icons/qwik-icon"
 import { ReactIcon } from "../icons/react-icon"
 import { SolidJSIcon } from "../icons/solidjs-icon"
-import { VueIcon } from "../icons/vue-icon"
 import { SvelteIcon } from "../icons/svelte-icon"
+import { VueIcon } from "../icons/vue-icon"
 
 export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 	{
@@ -40,7 +40,7 @@ export const FRAMEWORK_RESOURCES = (bigSize = false): LinkCardProps[] => [
 		Icon(props) {
 			return <SolidJSIcon {...props} />
 		},
-		backgroundColor: "#2B4E8B",
+		backgroundColor: "#446b9e",
 		bigSize,
 	},
 	{
