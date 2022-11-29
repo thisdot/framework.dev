1d939e3f68f2bdf0462efb5a1667524c50404c6e
Tyrel Chambers <tychambers3@gmail.com>
2022-11-29 10:53:09 -0500

    add tools

--- packages/site/src/data/deno/tools.ts
+++ packages/site/src/data/deno/tools.ts
@@ -1,5 +1,16 @@
 import { Tool } from "@framework/system/src/models/tool"
 
-export const toolTags = [] as const
+export const toolTags = ["vscode"] as const
 
-export const tools: Tool<typeof toolTags[number]>[] = []
+export const tools: Tool<typeof toolTags[number]>[] = [
+	{
+		name: "Deno VSCode",
+		description:
+			"This extension adds support for using Deno with Visual Studio Code, powered by the Deno language server",
+		author: "DenoLand",
+		href: "https://marketplace.visualstudio.com/items?itemName=denoland.vscode-deno",
+		image:
+			"https://denoland.gallerycdn.vsassets.io/extensions/denoland/vscode-deno/3.14.1/1668177865958/Microsoft.VisualStudio.Services.Icons.Default",
+		tags: ["vscode"],
+	},
+]
