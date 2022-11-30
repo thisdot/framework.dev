0864e0788ba5cc1e519ab4145d7a45a5afd74a4a
LindaKat <lindakt16@gmail.com>
2022-11-30 16:40:18 -0600

    test: add new echo to check

--- build-ignore.sh
+++ build-ignore.sh
@@ -1,5 +1,6 @@
 echo "Checking for framework match"
 echo git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react
+echo $PUBLIC_FRAMEWORK
 
 if [[ $PUBLIC_FRAMEWORK -eq "react" && git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react -eq 1 ]] 
 then
