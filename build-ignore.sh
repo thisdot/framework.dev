689075693ccd28b76ce41e0ba7e98c7d2e420f19
LindaKat <lindakt16@gmail.com>
2022-11-30 16:58:43 -0600

    test: changed equality check

--- build-ignore.sh
+++ build-ignore.sh
@@ -2,7 +2,7 @@ echo "Checking for framework match"
 echo $(git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react)
 echo $PUBLIC_FRAMEWORK
 
-if [ $PUBLIC_FRAMEWORK -eq "react" ] && [ $(git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react) -eq 1 ]
+if [ $PUBLIC_FRAMEWORK == "react" ] && [ $(git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react) -eq 1 ]
 then
   exit 1
 else 
