954826029e0cd341ac94959a7ae859f65952aac2
LindaKat <lindakt16@gmail.com>
2022-11-30 16:56:15 -0600

    test: more fixes to script file

--- build-ignore.sh
+++ build-ignore.sh
@@ -1,10 +1,10 @@
 echo "Checking for framework match"
-echo git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react
+echo $(git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react)
 echo $PUBLIC_FRAMEWORK
 
-if [[ $PUBLIC_FRAMEWORK -eq "react" && git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react -eq 1 ]] 
+if [ $PUBLIC_FRAMEWORK -eq "react" ] && [ $(git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react) -eq 1 ]
 then
-  process.exitCode = 1;
+  exit 1
 else 
-  process.exitCode = 0;
+  exit 0
 fi
