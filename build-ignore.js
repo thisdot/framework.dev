9013e5dd35ba6c78d1d9ae295e6c638f9984c83d
LindaKat <lindakt16@gmail.com>
2022-11-30 17:30:56 -0600

    test: added console messages for clarity

--- build-ignore.js
+++ build-ignore.js
@@ -2,7 +2,9 @@ console.log("Checking for framework match");
 
 switch (process.env.PUBLIC_FRAMEWORK) {
   case 'react':
+    console.log('framework match found, checking diff for changes')
     return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react"
   default:
+    console.log('no matches, exiting build')
     return process.exitCode = 0
 } 
