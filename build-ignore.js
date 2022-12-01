746e346739d35561624b3dd0d18d428928a62690
LindaKat <lindakt16@gmail.com>
2022-11-30 18:28:42 -0600

    feat: added all sites to build script; removed unreachable configs

--- build-ignore.js
+++ build-ignore.js
@@ -1,10 +1,37 @@
-console.log("Checking for framework match");
+console.log("Checking if the site needs to build...")
 
-switch (process.env.PUBLIC_FRAMEWORK) {
-  case 'react':
-    console.log('framework match found, checking diff for changes')
-    return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react"
-  default:
-    console.log('no matches, exiting build')
-    return process.exitCode = 0
-} 
+switch (process.env.PUBLIC_SITE_NAME) {
+	case "system":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/system"
+	case "landing":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/landing-site"
+	case "react":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "nodejs":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/nodejs ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "graphql":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/graphql ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "vue":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/vue ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "solidjs":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/solidjs ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "angular":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/angular ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "svelte":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/svelte ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	case "qwik":
+		console.log("site match found, checking diff for changes")
+		return "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/qwik ./packages/site/src/components ./packages/site/src/layouts ./packages/site/src/pages"
+	default:
+		console.log("no matches, exiting build process")
+		return (process.exitCode = 0)
+}
