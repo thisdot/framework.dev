568893fbdc347de9268b183523cfeba08732fd37
LindaKat <lindakt16@gmail.com>
2022-11-30 18:34:16 -0600

    cleanup: removed unused configs

--- packages/site/build-ignore.js
+++
@@ -1,6 +0,0 @@
-console.log("git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./src/data/react")
-if ($SNOWPACK_PUBLIC_FRAMEWORK === "react" && "git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ./src/data/react" === 1) {
-  process.exitCode = 1;
-} else {
-  process.exitCode = 0;
-}
