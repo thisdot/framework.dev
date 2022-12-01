a7ebc86cd4628d58854d60e62018600856991a19
dariodjuric <dario.djuric@gmail.com>
2022-12-01 13:11:29 +0100

    Chore/add webstorm plugin (#518)
    * Add WebStorm
    
    * Fix missing image

--- packages/site/src/data/deno/tools.ts
+++ packages/site/src/data/deno/tools.ts
@@ -1,6 +1,6 @@
 import { Tool } from "@framework/system/src/models/tool"
 
-export const toolTags = ["vscode"] as const
+export const toolTags = ["vscode", "webstorm"] as const
 
 export const tools: Tool<typeof toolTags[number]>[] = [
 	{
@@ -12,5 +12,15 @@ export const tools: Tool<typeof toolTags[number]>[] = [
 		image:
 			"https://denoland.gallerycdn.vsassets.io/extensions/denoland/vscode-deno/3.14.1/1668177865958/Microsoft.VisualStudio.Services.Icons.Default",
 		tags: ["vscode"],
+	},
+	{
+		name: "WebStorm Plugin for Deno",
+		description:
+			"This plugin adds support for using Deno with JetBrains WebStorm.",
+		author: "JetBrains",
+		href: "https://plugins.jetbrains.com/plugin/14382-deno",
+		image:
+			"https://plugins.jetbrains.com/static/versions/24971/jetbrains-simple.svg",
+		tags: ["webstorm"],
 	},
 ]
