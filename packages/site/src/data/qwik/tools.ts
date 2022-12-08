77e3f81397297eb394fb114ca3a1cd1c8c56ad4f
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2022-12-08 14:59:35 +0000

    Remove GraphQL Tools From Qwik Tools (#550)

--- packages/site/src/data/qwik/tools.ts
+++ packages/site/src/data/qwik/tools.ts
@@ -18,24 +18,6 @@ export const tools: Tool<typeof toolTags[number]>[] = [
 		tags: ["development"],
 	},
 	{
-		name: "GraphQL Playground for Chrome",
-		author: "Dustin R. Callaway",
-		description: "Chrome extension for GraphQL Playground",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg",
-		href: "https://chrome.google.com/webstore/detail/graphql-playground-for-ch/kjhjcgclphafojaeeickcokfbhlegecd?hl=en",
-		tags: ["Chrome"],
-	},
-	{
-		name: "GraphQL Playground for Firefox",
-		author: "Francesco De Stefano",
-		description: "Firefox extension for GraphQL Playground",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg",
-		href: "https://addons.mozilla.org/en-US/firefox/addon/graphql-developer-tools/",
-		tags: ["Firefox"],
-	},
-	{
 		name: "TanStack Router",
 		author: "Tanner Linsley",
 		description: "A fully typesafe router with first-class search-param APIs and built-in caching, built for JS/TS",
