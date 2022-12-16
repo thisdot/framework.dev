0b6cca94c89cf3af0d60a745358a94231eedb2fa
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-12-16 11:47:19 +0100

    feat: 574 contributors banner improvements
    * format

--- packages/landing-site/src/components/contributor-banner.tsx
+++ packages/landing-site/src/components/contributor-banner.tsx
@@ -1,7 +1,10 @@
-import {ContributorBanner as _ContributorBanner} from '@framework/system/src/components/homepage/contributor-banner'
-import {ContributorData} from '@framework/system/src/components/homepage/contributor'
+import { ContributorBanner as _ContributorBanner } from "@framework/system/src/components/homepage/contributor-banner"
+import { ContributorData } from "@framework/system/src/components/homepage/contributor"
 
-
-export default function ContributorBanner({contributors}:{contributors: ContributorData[]}) {
-    return (<_ContributorBanner contributors={contributors}/>)
+export default function ContributorBanner({
+	contributors,
+}: {
+	contributors: ContributorData[]
+}) {
+	return <_ContributorBanner contributors={contributors} />
 }
