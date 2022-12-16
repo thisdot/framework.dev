ecee2f5145842b9690e8d69fa52da417621b3540
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-12-16 11:34:01 +0100

    feat: 574 contributors banner improvements
    * fix ContributorBanner story
    * add Contributor story

--- packages/system/src/components/homepage/contributor-banner.stories.tsx
+++ packages/system/src/components/homepage/contributor-banner.stories.tsx
@@ -8,10 +8,27 @@ export default {
 	title: "Home/Contributor Banner",
 	component: ContributorBannerComponent,
 	args: {
-		contributorImages: [
-			"https://github.com/jbachhardie.png",
-			"https://github.com/tvanantwerp.png",
-			"https://github.com/markshenouda.png",
+		contributors: [
+			{
+				login: "jbachhardie",
+				url: "https://github.com/jbachhardie",
+				avatarUrl: "https://github.com/jbachhardie.png",
+			},
+			{
+				login: "tvanantwerp",
+				url: "https://github.com/tvanantwerp",
+				avatarUrl: "https://github.com/tvanantwerp.png",
+			},
+			{
+				login: "markshenouda",
+				url: "https://github.com/markshenouda",
+				avatarUrl: "https://github.com/markshenouda.png",
+			},
+			{
+				login: "ktrz",
+				url: "https://github.com/ktrz",
+				avatarUrl: "https://github.com/ktrz.png",
+			},
 		],
 	},
 } as Meta
---
+++ packages/system/src/components/homepage/contributor.stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from "@storybook/react"
+import {
+	Contributor as ContributorComponent,
+	ContributorProps,
+} from "./contributor"
+
+export default {
+	title: "Home/Contributor",
+	component: ContributorComponent,
+	args: {
+		contributor: {
+			login: "ktrz",
+			url: "https://github.com/ktrz",
+			avatarUrl: "https://github.com/ktrz.png",
+		},
+	},
+} as Meta
+
+const Template: Story<ContributorProps> = (args) => (
+	<ContributorComponent {...args} />
+)
+
+export const Contributor = Template.bind({})
