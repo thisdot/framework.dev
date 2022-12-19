41a01e24842ea3365c6b265dfa489ade6f805af5
Dario Djuric <dario@thisdot.co>
2022-12-19 12:54:41 +0100

    Add Deno

--- packages/site/src/data/search-data.ts
+++ packages/site/src/data/search-data.ts
@@ -1,28 +1,18 @@
-import { AllCategories } from '@framework/system/src/models/all-categories'
-import { blogIndexMetadata } from '@framework/system/src/models/blog'
-import { bookIndexMetadata } from '@framework/system/src/models/book'
-import { communityIndexMetadata } from '@framework/system/src/models/community'
-import { courseIndexMetadata } from '@framework/system/src/models/course'
-import { libraryIndexMetadata } from '@framework/system/src/models/library'
-import { podcastIndexMetadata } from '@framework/system/src/models/podcast'
-import { toolIndexMetadata } from '@framework/system/src/models/tool'
+import { AllCategories } from "@framework/system/src/models/all-categories"
+import { blogIndexMetadata } from "@framework/system/src/models/blog"
+import { bookIndexMetadata } from "@framework/system/src/models/book"
+import { communityIndexMetadata } from "@framework/system/src/models/community"
+import { courseIndexMetadata } from "@framework/system/src/models/course"
+import { libraryIndexMetadata } from "@framework/system/src/models/library"
+import { podcastIndexMetadata } from "@framework/system/src/models/podcast"
+import { toolIndexMetadata } from "@framework/system/src/models/tool"
 
 const SubCategoryDictionary = new Map<string, string[]>()
-	.set('graphql', ['Servers', 'Client', 'Schema'])
-	.set("nodejs", [
-		"framework",
-		"data fetching",
-		"API",
-	])
+	.set("graphql", ["Servers", "Client", "Schema"])
+	.set("nodejs", ["framework", "data fetching", "API"])
 	.set("qwik", ["state management", "internationalization", "data fetching"])
-	.set("deno", [
-		"drivers",
-		"frameworks",
-		"utilities",
-		"tooling",
-		"web servers",
-	])
- 
+	.set("deno", ["drivers", "frameworks", "utilities", "tooling", "web servers"])
+
 export async function getSearchData(
 	framework: string
 ): Promise<AllCategories[]> {
@@ -42,15 +32,16 @@ export async function getSearchData(
 			indexMetadata: libraryIndexMetadata,
 			name: libraryIndexMetadata.name,
 			tags: libraryTags,
-			subCategories: SubCategoryDictionary.get(framework)
-				|| ([
-					'state management',
-					'data fetching',
-					'styling',
-					'component library',
-					'forms',
-					'framework',
-					'internationalization',
+			subCategories:
+				SubCategoryDictionary.get(framework) ||
+				([
+					"state management",
+					"data fetching",
+					"styling",
+					"component library",
+					"forms",
+					"framework",
+					"internationalization",
 				] as typeof libraryTags[number][]),
 		},
 		{
@@ -71,8 +62,8 @@ export async function getSearchData(
 			name: communityIndexMetadata.name,
 			tags: communityTags,
 			subCategories: [
-				'meetups',
-				'conferences',
+				"meetups",
+				"conferences",
 			] as typeof communityTags[number][],
 		},
 		{
