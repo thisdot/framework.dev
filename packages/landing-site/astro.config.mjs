bddaebd633b6e7dd9afd27dd9bbd441aba7616a3
Dane Grant <danecando@gmail.com>
2023-01-03 15:10:07 -0500

    chore: landing site running on astro 0.23

--- packages/landing-site/astro.config.mjs
+++ packages/landing-site/astro.config.mjs
@@ -1,13 +1,12 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
+
 export default /** @type {import('astro').AstroUserConfig} */ {
 	// Enable the React renderer to support React JSX components.
 	renderers: ["@astrojs/renderer-react"],
 	buildOptions: {
-		pageUrlFormat: "file",
-		site: `https://${process.env.SNOWPACK_PUBLIC_FRAMEWORK}.framework.dev`,
+		site: `https://${process.env.VITE_FRAMEWORK}.framework.dev`,
 	},
-	devOptions: {
-		// If pageUrlFormat is "file" this line needs to be present for it to build correctly.
-		// This is a bug in Astro: https://github.com/withastro/astro/issues/1338
-		trailingSlash: "never",
+	vite: {
+		plugins: [vanillaExtractPlugin()],
 	},
 }
