7376c6dd18121d884f510d7c5f189d9ec6764458
LindaKat <lindakt16@gmail.com>
2023-01-04 10:27:53 -0600

    test: passing git command to exec function

--- build-ignore.js
+++ build-ignore.js
@@ -1,42 +1,55 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { exec } = require('node:child_process');
+
 console.log("Checking if the site needs to build...");
 
-const HEAD = 'main';
 const COMMIT = process.env.COMMIT_REF;
-
-console.log(HEAD, COMMIT)
+let gitDiff = '';
 
 switch (process.env.PUBLIC_SITE_NAME) {
 	case "system":
-		console.log("site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/system/`
-	case "landing":
-		console.log("landing site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/landing-site/`
-	case "react":
-		console.log("react site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/react/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "nodejs":
-		console.log("node site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/nodejs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "graphql":
-		console.log("graphql site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/graphql/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "vue":
-		console.log("vue site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/vue/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "solidjs":
-		console.log("solid site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/solidjs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "angular":
-		console.log("angular site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/angular/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "svelte":
-		console.log("svelte site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/svelte/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
-	case "qwik":
-		console.log("qwik site match found, checking diff for changes")
-		return `git diff --quiet ${HEAD} ${COMMIT} ./packages/site/src/data/qwik/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`
+		console.log("site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/system/`;
+    break;
+  case "landing":
+		console.log("landing site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/landing-site/`;
+    break;
+  case "react":
+		console.log("react site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/react/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "nodejs":
+		console.log("node site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/nodejs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "graphql":
+		console.log("graphql site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/graphql/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "vue":
+		console.log("vue site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/vue/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "solidjs":
+		console.log("solid site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/solidjs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "angular":
+		console.log("angular site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/angular/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "svelte":
+		console.log("svelte site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/svelte/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
+  case "qwik":
+		console.log("qwik site match found, checking diff for changes");
+		gitDiff = `git diff --quiet main ${COMMIT} ./packages/site/src/data/qwik/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    break;
 	default:
-		console.log("no matches, exiting build process")
-		return (process.exitCode = 0)
+		console.log("no matches, exiting build process");
+		return (process.exitCode = 0);
 }
+
+return exec(gitDiff);
