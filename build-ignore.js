0ef0250b058863e671a98afec5210cceff399a43
LindaKat <lindakt16@gmail.com>
2023-01-03 18:32:06 -0600

    test: added root slash back to command, more detailed logging in script

--- build-ignore.js
+++ build-ignore.js
@@ -5,31 +5,31 @@ switch (process.env.PUBLIC_SITE_NAME) {
 		console.log("site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/system/"
 	case "landing":
-		console.log("site match found, checking diff for changes")
+		console.log("landing site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/landing-site/"
 	case "react":
-		console.log("site match found, checking diff for changes")
+		console.log("react site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/react/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "nodejs":
-		console.log("site match found, checking diff for changes")
+		console.log("node site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/nodejs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "graphql":
-		console.log("site match found, checking diff for changes")
+		console.log("graphql site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/graphql/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "vue":
-		console.log("site match found, checking diff for changes")
+		console.log("vue site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/vue/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "solidjs":
-		console.log("site match found, checking diff for changes")
+		console.log("solid site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/solidjs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "angular":
-		console.log("site match found, checking diff for changes")
+		console.log("angular site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/angular/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "svelte":
-		console.log("site match found, checking diff for changes")
+		console.log("svelte site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/svelte/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	case "qwik":
-		console.log("site match found, checking diff for changes")
+		console.log("qwik site match found, checking diff for changes")
 		return "git diff $CACHED_COMMIT_REF $COMMIT_REF ./packages/site/src/data/qwik/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/"
 	default:
 		console.log("no matches, exiting build process")
