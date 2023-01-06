1803e896b5ec643ba196f95828405a0f2b421722
Linda Thompson <lindakt16@gmail.com>
2023-01-06 17:24:28 -0600

    fix: update build script to use new var name and check for config changes; update contributing doc

--- build-ignore.js
+++ build-ignore.js
@@ -6,7 +6,7 @@ console.log("Checking if the site needs to build...");
 const COMMIT = process.env.COMMIT_REF;
 let gitDiff = '';
 
-switch (process.env.PUBLIC_SITE_NAME) {
+switch (process.env.VITE_SITE_NAME) {
 	case "system":
 		console.log("site match found, checking diff for changes");
 		gitDiff = `git diff main ${COMMIT} ./packages/system/`;
@@ -17,39 +17,39 @@ switch (process.env.PUBLIC_SITE_NAME) {
     break;
   case "react":
 		console.log("react site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/react/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/react/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "nodejs":
 		console.log("node site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/nodejs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/nodejs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "graphql":
 		console.log("graphql site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/graphql/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/graphql/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "vue":
 		console.log("vue site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/vue/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/vue/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "angular":
     console.log("angular site match found, checking diff for changes");
-    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/angular/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/angular/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "svelte":
     console.log("svelte site match found, checking diff for changes");
-    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/svelte/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/svelte/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "solidjs":
 		console.log("solid site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/solidjs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/solidjs/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "qwik":
 		console.log("qwik site match found, checking diff for changes");
-		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/qwik/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+		gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/qwik/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
   case "deno":
     console.log("deno site match found, checking diff for changes");
-    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/deno/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/`;
+    gitDiff = `git diff main ${COMMIT} ./packages/site/src/data/deno/ ./packages/site/src/components/ ./packages/site/src/layouts/ ./packages/site/src/pages/ .packages/site/*.*`;
     break;
 	default:
 		console.log("no matches, exiting build process");
