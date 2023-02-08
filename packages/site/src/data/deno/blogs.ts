2a21d9372e336eafbeec50cddf4651c52e068449
morgnism <morgan@border.dev>
2023-02-08 14:17:56 -0500

    sorted deno blogs

--- packages/site/src/data/deno/blogs.ts
+++ packages/site/src/data/deno/blogs.ts
@@ -2,16 +2,7 @@ import { Blog } from "@framework/system/src/models/blog"
 
 export const blogTags = [] as const
 
-export const blogs: Blog<typeof blogTags[number]>[] = [
-	{
-		title: "This Dot Blog",
-		author: "This Dot Labs",
-		description:
-			"Variety of topics related to JavaScript, written by the team at This Dot",
-		image: "https://github.com/thisdot.png",
-		href: "https://www.thisdot.co/blog/",
-		tags: [],
-	},
+export const blogs: Blog<(typeof blogTags)[number]>[] = [
 	{
 		title: "Deno Blog",
 		author: "Deno",
@@ -32,24 +23,6 @@ export const blogs: Blog<typeof blogTags[number]>[] = [
 		tags: [],
 	},
 	{
-		title: "Ultimate Courses - Deno blogs",
-		author: "Ultimate Courses",
-		description: "All about Deno, the new JavaScript and TypeScript runtime.",
-		image:
-			"https://ultimatecourses.com/assets/logo-ef24a2d3b6a0febba9ff80a1b01d632db750feb083442d0071dff7426762e0c2.svg",
-		href: "https://ultimatecourses.com/blog/category/deno/",
-		tags: [],
-	},
-	{
-		title: "LogRocket Blog",
-		author: "LogRocket",
-		description: "This is a blog dedicated to all things web development",
-		image:
-			"https://pbs.twimg.com/profile_images/1326191211850440708/uQc_hHbU_400x400.jpg",
-		href: "https://blog.logrocket.com/tag/deno/",
-		tags: [],
-	},
-	{
 		title: "The Deno handbook",
 		author: "freeCodeCamp",
 		description: "A TypeScript Runtime Tutorial with Code Examples",
@@ -64,6 +37,33 @@ export const blogs: Blog<typeof blogTags[number]>[] = [
 			"Deno.js is the better Node.js. Or, at least, that's the goal. Here's what sets Deno apart from Node and my opinion on whether you should make the switch.",
 		image: "https://github.com/Academind.png",
 		href: "https://academind.com/tutorials/denojs-first-look",
+		tags: [],
+	},
+	{
+		title: "Ultimate Courses - Deno blogs",
+		author: "Ultimate Courses",
+		description: "All about Deno, the new JavaScript and TypeScript runtime.",
+		image:
+			"https://ultimatecourses.com/assets/logo-ef24a2d3b6a0febba9ff80a1b01d632db750feb083442d0071dff7426762e0c2.svg",
+		href: "https://ultimatecourses.com/blog/category/deno/",
+		tags: [],
+	},
+	{
+		title: "This Dot Blog",
+		author: "This Dot Labs",
+		description:
+			"Variety of topics related to JavaScript, written by the team at This Dot",
+		image: "https://github.com/thisdot.png",
+		href: "https://www.thisdot.co/blog/",
+		tags: [],
+	},
+	{
+		title: "LogRocket Blog",
+		author: "LogRocket",
+		description: "This is a blog dedicated to all things web development",
+		image:
+			"https://pbs.twimg.com/profile_images/1326191211850440708/uQc_hHbU_400x400.jpg",
+		href: "https://blog.logrocket.com/tag/deno/",
 		tags: [],
 	},
 ]
