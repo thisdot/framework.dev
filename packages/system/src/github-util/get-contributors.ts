4ac1ea576bb2a812291a35ce1cc39a0128f8cc21
Dane Grant <danecando@gmail.com>
2023-02-10 09:31:15 -0500

    feat: adds retry functionality to getContributorsData for periodic failures

--- packages/system/src/github-util/get-contributors.ts
+++ packages/system/src/github-util/get-contributors.ts
@@ -1,3 +1,4 @@
+import pRetry, { AbortError } from "p-retry"
 import { ContributorData } from "../components/homepage/contributor"
 
 interface ContributorApiData {
@@ -6,20 +7,41 @@ interface ContributorApiData {
 	avatar_url: string
 }
 
-export const getContributorsData = (): Promise<ContributorData[]> => {
-	return fetch(
-		"https://api.github.com/repos/thisdot/framework.dev/contributors?anon=1"
-	)
-		.then((res) => res.json())
-		.then((data: ContributorApiData[]) => {
-			try {
-				return data.map((user) => ({
-					login: user.login,
-					url: user.html_url,
-					avatarUrl: user.avatar_url,
-				}))
-			} catch (e) {
-				return []
-			}
+export const getContributorsData = async (): Promise<ContributorData[]> => {
+	const runFetch = async () => {
+		const abortError = new AbortError("Failed to fetch contributors")
+		const response = await fetch(
+			"https://api.github.com/repos/thisdot/framework.dev/contributors?anon=1"
+		)
+		if (!response.ok) {
+			throw abortError
+		}
+		const data = await response.json()
+		if (!data) {
+			throw abortError
+		}
+
+		return data
+	}
+
+	try {
+		const data = await pRetry(runFetch, {
+			retries: 3,
+			onFailedAttempt: (error) => {
+				console.log(
+					`getContributorsData Failure: Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`
+				)
+			},
 		})
+
+		return data.map((user: ContributorApiData) => ({
+			login: user.login,
+			url: user.html_url,
+			avatarUrl: user.avatar_url,
+		}))
+	} catch (error) {
+		throw new Error(
+			"Failed to fetch contributors. Please try rebuilding the website."
+		)
+	}
 }
