7f267a6a598cf02cf240cf70d1bf0bb356a3dd8e
Jerry Hogan <jerry.hogan@thisdot.co>
2023-03-24 16:43:35 +0100

    Added solidjs router resource (#757)
    * Added solidjs router resource
    
    * SolidJs to SolidJS

--- packages/site/src/data/solidjs/libraries.ts
+++ packages/site/src/data/solidjs/libraries.ts
@@ -9,7 +9,7 @@ const defaultImage =
 export const libraries: Library[] = [
 	{
 		name: 'Solid Store',
-		author: 'SolidJs',
+		author: 'SolidJS',
 		repo: 'https://www.github.com/solidjs/solid/tree/main/packages/solid/store',
 		package: '',
 		href: 'https://www.solidjs.com/tutorial/stores_createstore',
@@ -59,7 +59,7 @@ export const libraries: Library[] = [
 	},
 	{
 		name: 'Solid Resources',
-		author: 'Solidjs',
+		author: 'SolidJS',
 		repo: 'https://github.com/solidjs/solid',
 		package: '',
 		href: 'https://www.solidjs.com/tutorial/async_resources',
@@ -135,7 +135,7 @@ export const libraries: Library[] = [
 		repo: 'https://www.github.com/DigiChanges/solid-multiselect',
 		package: 'https://www.npmjs.com/package/@digichanges/solid-multiselect',
 		href: 'https://github.com/DigiChanges/solid-multiselect',
-		description: 'A multi-select dropdown implementation for SolidJs',
+		description: 'A multi-select dropdown implementation for SolidJS',
 		image:
 			'https://opengraph.githubassets.com/3d77a1fbc9cc60af77bbd066466a010b7fd84422c2564802223a006317e81e7e/DigiChanges/solid-multiselect',
 		tags: [LibraryTag.COMPONENT],
@@ -160,7 +160,7 @@ export const libraries: Library[] = [
 		repo: 'https://www.github.com/swordev/suid',
 		package: 'https://www.npmjs.com/package/suid',
 		href: 'https://suid.io/',
-		description: 'A port of MUI built with SolidJs',
+		description: 'A port of MUI built with SolidJS',
 		image: 'https://suid.io/assets/preview.png',
 		tags: [LibraryTag.COMPONENT],
 		language: 'TypeScript',
@@ -180,7 +180,7 @@ export const libraries: Library[] = [
 	},
 	{
 		name: 'Solid Start',
-		author: 'solidjs',
+		author: 'SolidJS',
 		repo: 'https://www.github.com/solidjs/solid-start',
 		package: '',
 		href: 'https://start.solidjs.com/getting-started/what-is-solidstart',
@@ -193,7 +193,7 @@ export const libraries: Library[] = [
 	},
 	{
 		name: 'Solid Form Handler',
-		author: 'solidjs',
+		author: 'SolidJS',
 		repo: 'https://www.github.com/webblocksapp/solid-form-handler',
 		package: 'https://www.npmjs.com/package/solid-form-handler',
 		href: 'https://solid-form-handler.com/docs/introduction',
@@ -238,6 +238,19 @@ export const libraries: Library[] = [
 		image: 'https://vitejs.dev/logo.svg',
 		href: 'https://github.com/vitejs/vite',
 		tags: [LibraryTag.TOOLING],
+		language: 'TypeScript',
+	},
+	{
+		name: 'Solid Router',
+		author: 'SolidJS',
+		repo: 'https://github.com/solidjs/solid-router',
+		package: '',
+		description:
+			'A universal router for Solid inspired by Ember and React Router',
+		image:
+			'https://opengraph.githubassets.com/41111dcd4fc2ba4f3f3c097212fa56ffc1e2e6abde27785dba293e8341132d49/solidjs/solid-router',
+		href: 'https://github.com/solidjs/solid-router',
+		tags: [LibraryTag.ROUTING],
 		language: 'TypeScript',
 	},
 	{
